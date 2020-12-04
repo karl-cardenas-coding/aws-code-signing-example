11214fba2872829c13eb61e856b6f1f3003ef47e
Karl Cardenas <cardenas88karl@gmail.com>
2020-12-04 08:45:24 -0700

    option 2

--- main.tf
+++ main.tf
@@ -60,10 +60,11 @@ resource "null_resource" "build_upload" {
   triggers = {
     time = timestamp()
   }
+
   provisioner "local-exec" {
     command = <<EOT
     zip lambda.zip ./lambda_function.py 
-    aws s3 cp --profile="home" lambda.zip s3://${var.code-bucket}/unsigned
+    aws s3 cp lambda.zip s3://${var.code-bucket}/unsigned
     EOT
   }
 }
