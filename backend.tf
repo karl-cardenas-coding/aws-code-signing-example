ceede93eb6a441b096f4b6feacc6ff80b23aefcc
Karl Cardenas <cardenas88karl@gmail.com>
2020-12-04 07:54:55 -0700

    init

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-statefiles-karl-cardenas-coding"
-    key    = "aws-code-signing-example/state.tfstate"
-    region = "us-east-1"
+    bucket   = "terraform-statefiles-karl-cardenas-coding"
+    key      = "aws-code-signing-example/state.tfstate"
+    region   = "us-east-1"
     role_arn = "arn:aws:iam::140040602879:role/automation-role"
-    encrypt        = true
+    encrypt  = true
   }
 }
