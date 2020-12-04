acbcb4930a55fb45eb34184fe5d3ec4edf83b469
Karl Cardenas <cardenas88karl@gmail.com>
2020-12-04 09:35:00 -0700

    option 3

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -e
 
 zip lambda.zip ./lambda_function.py 
-aws s3 cp lambda.zip s3://${var.code-bucket}/unsigned
+aws s3 cp lambda.zip s3://code-signing-demo/unsigned
 aws signer start-signing-job --profile-name="demo"  --source "s3={bucketName='code-signing-demo',key='unsigned/lambda.zip',version="null"}" --destination "s3={bucketName='code-signing-demo',prefix=signed/lambda-}"
