6dd99cc1b2cc39ffd7e72c388b66da47ec862e17
mrspanishviking <cardenas88karl@gmail.com>
2020-12-05 18:16:27 -0700

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket   = "terraform-statefiles-karl-cardenas-coding"
-    key      = "aws-code-signing-example/state.tfstate"
-    region   = "us-east-1"
-    role_arn = "arn:aws:iam::140040602879:role/automation-role"
-    encrypt  = true
-  }
-}
